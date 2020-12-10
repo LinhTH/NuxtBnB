8277c8019d72ab12080c476b3f4d854fd8dbb233
Linh Truong <thlinh8392@outlook.com>
2020-12-11 06:51:33 +0700

    feat: add first routing path

---
+++ pages/home/_id.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>{{ home.title }}</div>
+</template>
+
+<script lang="ts">
+import homes from '~/data/homes'
+
+export default {
+  data() {
+    return {
+      home: {},
+    }
+  },
+  // In Nuxt, the method is ran on both the server and on the client on the first page view
+  created() {
+    this.home = homes.find((home) => home.objectID === this.$route?.params?.id)
+  },
+}
+</script>
