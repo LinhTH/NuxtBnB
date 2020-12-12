00def45918eeb302133157fad8d54969cfffa098
Linh Truong <thlinh8392@outlook.com>
2020-12-12 15:33:05 +0700

    feat: update routing

--- pages/home/_id.vue
+++ pages/home/_id.vue
@@ -1,22 +1,47 @@
 <template>
-  <div>{{ home.title }}</div>
+  <div>
+    <div style="display: flex">
+      <img
+        v-for="image in home.images"
+        :key="image"
+        :src="image"
+        width="200"
+        height="150"
+      />
+    </div>
+    {{ home.title }} <br />
+    {{ home.pricePerNight }} / night <br />
+    <img src="/images/marker.svg" width="20" height="20" />{{
+      home.location.address
+    }}
+    {{ home.location.city }}
+    {{ home.location.state }}
+    {{ home.location.country }}<br />
+    <img src="/images/star.svg" width="20" height="20" />
+    {{ home.reviewValue }} <br />
+    {{ home.guests }} guests, {{ home.bedrooms }} rooms, {{ home.beds }} beds,
+    {{ home.bathrooms }} baths <br />
+    <nuxt-link :to="`/`">Home</nuxt-link>
+  </div>
 </template>
 
-<script lang="ts">
-import Vue from 'vue'
+<script>
 import homes from '~/data/homes.json'
 
-export default Vue.extend({
+export default {
   data() {
     return {
-      home: {} as any,
+      home: {},
     }
   },
   // In Nuxt, the method is ran on both the server and on the client on the first page view
   created() {
-    this.home = homes.find(
-      (home: any) => home.objectID === this.$route?.params?.id
-    )
+    this.home = homes.find((home) => home.objectID === this.$route?.params?.id)
   },
-})
+  head() {
+    return {
+      title: this.home.title,
+    }
+  },
+}
 </script>
