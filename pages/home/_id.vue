cd4b49221e79bc580723c11393e05ee08aa157a8
Linh Truong <thlinh8392@outlook.com>
2020-12-13 11:30:38 +0700

    feat: integrate Google Maps

--- pages/home/_id.vue
+++ pages/home/_id.vue
@@ -7,6 +7,7 @@
         :src="image"
         width="200"
         height="150"
+        alt="home image"
       />
     </div>
     {{ home.title }} <br />
@@ -21,14 +22,21 @@
     {{ home.reviewValue }} <br />
     {{ home.guests }} guests, {{ home.bedrooms }} rooms, {{ home.beds }} beds,
     {{ home.bathrooms }} baths <br />
-    <nuxt-link :to="`/`">Home</nuxt-link>
+    <div ref="map" style="height: 800px; width: 800px"></div>
   </div>
 </template>
 
 <script>
 import homes from '~/data/homes.json'
 
+if (process.client) {
+  window.initMap = function () {
+    console.log(test)
+  }
+}
+
 export default {
+  layout: 'red',
   data() {
     return {
       home: {},
@@ -38,9 +46,43 @@ export default {
   created() {
     this.home = homes.find((home) => home.objectID === this.$route?.params?.id)
   },
+  mounted() {
+    const mapOptions = {
+      zoom: 18,
+      center: new window.google.maps.LatLng(
+        this.home._geoloc.lat,
+        this.home._geoloc.lng
+      ),
+    }
+    const map = new window.google.maps.Map(this.$refs.map, mapOptions)
+    const position = new window.google.maps.LatLng(
+      this.home._geoloc.lat,
+      this.home._geoloc.lng
+    )
+    const marker = new window.google.maps.Marker({
+      position,
+    })
+    marker.setMap(map)
+  },
   head() {
     return {
       title: this.home.title,
+      script: [
+        {
+          src:
+            'https://maps.googleapis.com/maps/api/js?key=AIzaSyCsKXrZB1kSA-Xw4FqmqhDNNHhP__Homtw&libraries=places&callback=initMap',
+          hid: 'map',
+          defer: true,
+          skip: process.client && window.mapLoaded, // Skip to avoid the error: you have included the Google Maps Javascript multiple times on this page
+        },
+        {
+          innerHTML: 'window.initMap = function() { window.mapLoaded = true }',
+          hid: 'map-init',
+        },
+      ],
+      __dangerouslyDisableSanitizersByTagID: {
+        'map-init': ['innerHTML'],
+      },
     }
   },
 }
