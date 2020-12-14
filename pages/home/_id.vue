18d7c9857920b4d8d37b12cb1d1120f8346d9808
Linh Truong <thlinh8392@outlook.com>
2020-12-14 23:06:04 +0700

    refactor: make code cleaner

--- pages/home/_id.vue
+++ pages/home/_id.vue
@@ -36,10 +36,17 @@ export default {
    * @param $dataApi
    * @returns {Promise<{home: *}>}
    */
-  async asyncData({ params, $dataApi }) {
-    const home = await $dataApi.getHome(params?.id)
+  async asyncData({ params, $dataApi, error }) {
+    const response = await $dataApi.getHome(params?.id)
+
+    if (!response.ok)
+      return error({
+        statusCode: response.status,
+        message: response.statusText,
+      })
+
     return {
-      home,
+      home: response.json,
     }
   },
   data() {
--- plugins/dataApi.js
+++ plugins/dataApi.js
@@ -1,22 +1,34 @@
 export default function (_context, inject) {
   const appId = '8DAUXPT4M7'
   const apiKey = '14863c0250f392e9df9b240257d24c28'
+  const headers = {
+    'X-Algolia-API-Key': apiKey,
+    'X-Algolia-Application-Id': appId,
+  }
 
   inject('dataApi', {
     getHome,
   })
 
   async function getHome(homeId) {
-    const response = await fetch(
-      `https://${appId}-dsn.algolia.net/1/indexes/homes/${homeId}`,
-      {
-        headers: {
-          'X-Algolia-API-Key': apiKey,
-          'X-Algolia-Application-Id': appId,
-        },
-      }
+    return unwrap(
+      await fetch(
+        `https://${appId}-dsn.algoliaa.net/1/indexes/homes/${homeId}`,
+        {
+          headers,
+        }
+      )
     )
+  }
+
+  async function unwrap(response) {
     const json = await response.json()
-    return json
+    const { ok, status, statusText } = response
+    return {
+      json,
+      ok,
+      status,
+      statusText,
+    }
   }
 }
